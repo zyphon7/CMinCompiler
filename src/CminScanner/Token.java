a9d6c165fc985146b370922e91fb56cb8cd96fe5
Spencer Pullins <gecko2010msp@yahoo.com>
2018-02-04 13:20:27 -0500

    Token Beginning

---
+++ src/CminScanner/Token.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CminScanner;
+
+/**
+ *
+ * @author Spencer
+ */
+public class Token {
+
+ 
+    
+    public enum TokenType {
+        INT_TOKEN,
+        DOUBLE_TOKEN,
+        IF_TOKEN,
+        // rest of tokens ....
+    }
+
+    
+    private TokenType tokenType;
+    private Object tokenData;
+
+    public Token (TokenType type) {
+        this (type, null);
+    }
+
+    public Token (TokenType type, Object data) {
+        tokenType = type;
+        tokenData = data;
+    }
+
+    
+       /**
+     * @return the tokenType
+     */
+    public TokenType getTokenType() {
+        return tokenType;
+    }
+
+    /**
+     * @param tokenType the tokenType to set
+     */
+    public void setTokenType(TokenType tokenType) {
+        this.tokenType = tokenType;
+    }
+
+    /**
+     * @return the tokenData
+     */
+    public Object getTokenData() {
+        return tokenData;
+    }
+
+    /**
+     * @param tokenData the tokenData to set
+     */
+    public void setTokenData(Object tokenData) {
+        this.tokenData = tokenData;
+    }
+    
+}
