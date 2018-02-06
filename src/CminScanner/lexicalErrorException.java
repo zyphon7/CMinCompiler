9567a27d474ed62df7cfa50754385e83ca369885
Pikapal101 <annie.pikapal@gmail.com>
2018-02-06 17:54:37 -0500

    lexical error

---
+++ src/CminScanner/lexicalErrorException.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CminScanner;
+
+/**
+ *
+ * @author Annie
+ */
+public class lexicalErrorException extends Exception {
+
+    public lexicalErrorException() {
+    }
+    
+    public lexicalErrorException(String e){
+        super(e);
+    }
+    
+}
