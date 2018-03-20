05779624e113a76405a9b7a1c9cb819b8a998ada
Pikapal101 <annie.pikapal@gmail.com>
2018-03-20 18:27:15 -0400

    compound statement

--- src/cminusparser/CompoundStmt.java
+++ src/cminusparser/CompoundStmt.java
@@ -5,21 +5,50 @@
  */
 package cminusparser;
 
+import CminScanner.Token;
+import CminScanner.Token.TokenType;
+import static cminusparser.CminParser.cminscanner;
+import static cminusparser.VarDecl.parseVarDecl;
+import java.util.ArrayList;
+
 /**
  *
  * @author Annie
  */
 public class CompoundStmt extends Statement{
     
+    private ArrayList<Declaration> varDecls = new ArrayList<Declaration>();
+    private ArrayList<Statement> stmtList = new ArrayList<Statement>();
     
-    private static String caller = "CompoundStmt";
+    private static final String caller = "CompoundStmt";
+
+    private static void matchToken(TokenType tokenType) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
     public CompoundStmt(){
         
     }
     
     private static Statement parseCompoundStmt(){
-        
+        CompoundStmt c = new CompoundStmt();
+        matchToken(TokenType.LCURLY);
+        while(cminscanner.viewNextToken().getTokenType() == TokenType.INT){
+            c.varDecls.add(parseVarDecl(null));
+        }
+        //statement
+        while(cminscanner.viewNextToken().getTokenType() == TokenType.ID ||
+              cminscanner.viewNextToken().getTokenType() == TokenType.LP ||
+              cminscanner.viewNextToken().getTokenType() == TokenType.NUM ||
+              cminscanner.viewNextToken().getTokenType() == TokenType.SEMICOLON ||
+              cminscanner.viewNextToken().getTokenType() == TokenType.IF ||
+              cminscanner.viewNextToken().getTokenType() == TokenType.WHILE ||
+              cminscanner.viewNextToken().getTokenType() == TokenType.RETURN ||
+              cminscanner.viewNextToken().getTokenType() == TokenType.LCURLY){
+            c.stmtList.add(parseStatement());
+        }
+        matchToken(TokenType.RCURLY);
+        return c;
     }
     
     void print(){
