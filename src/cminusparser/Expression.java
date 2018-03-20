6a2e1d9974fbbf30ad714522a7a184b6404aecde
Pikapal101 <annie.pikapal@gmail.com>
2018-03-20 19:05:22 -0400

    expression functions started

--- src/cminusparser/Expression.java
+++ src/cminusparser/Expression.java
@@ -5,26 +5,51 @@
  */
 package cminusparser;
 
+import CminScanner.Token;
+import CminScanner.Token.TokenType;
+import static cminusparser.CminParser.cminscanner;
+import static cminusparser.CminParser.matchToken;
+
 /**
  *
  * @author Annie
  */
 public abstract class Expression {
     
-    static Expression parseExpression(){
+    private static String caller = "EXPRESSION";
+    
+    static Expression parseExpression(Token t){
+        Token oldTok = null;
+        if(t == null){
+            if(cminscanner.viewNextToken().getTokenType() == TokenType.ID){
+                oldTok = matchToken(TokenType.ID, caller);
+                return parseExpressionPrime(oldTok);
+            }
+            else if(cminscanner.viewNextToken().getTokenType() == TokenType.LP){
+                oldTok = matchToken(TokenType.LP, caller);
+                return parseExpression(oldTok);
+            }
+            else if(cminscanner.viewNextToken().getTokenType() == TokenType.NUM){
+                oldTok = matchToken(TokenType.NUM, caller);
+                return parseSimpleExpr(oldTok);    
+            }
+        else{
+            //error and exit
+        }
+        return null;
+    }
+}
+    
+    static Expression parseExpressionPrime(Token t){
         return null;
     }
     
-    static Expression parseExpressionPrime(){
-        
-    }
-    
     static Expression parseExpressionDoublePrime(){
-        
+        return null;
     }
     
-    static Expression parseSimpleExpr(){
-        
+    static Expression parseSimpleExpr(Token t){
+        return null;
     }
     
     abstract void print();
