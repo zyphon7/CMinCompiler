a93b7dbd99741c3c23d9dda7d3f52f21955932d7
Spencer Pullins <gecko2010msp@yahoo.com>
2018-03-20 20:26:27 -0400

    Finished Statement

--- src/cminusparser/Statement.java
+++ src/cminusparser/Statement.java
@@ -7,7 +7,11 @@ package cminusparser;
 
 import CminScanner.Token.TokenType;
 import static cminusparser.CminParser.cminscanner;
+import static cminusparser.CompoundStmt.parseCompoundStmt;
 import static cminusparser.ExpressionStmt.parseExpressionStmt;
+import static cminusparser.IterationStmt.parseIterationStmt;
+import static cminusparser.ReturnStmt.parseReturnStmt;
+import static cminusparser.SelectionStmt.parseSelectionStmt;
 
 /**
  *
@@ -16,12 +20,27 @@ import static cminusparser.ExpressionStmt.parseExpressionStmt;
 public abstract class Statement {
     
     static Statement parseStatement(){
-        Statement s = null;
         TokenType t = cminscanner.viewNextToken().getTokenType();
         //expression-stmt
         if(t == TokenType.ID || t == TokenType.LP || t == TokenType.NUM ||
                 t == TokenType.SEMICOLON){
-            s = parseExpressionStmt();
+            return parseExpressionStmt();
+        }
+        else if(t == TokenType.LCURLY){
+            return parseCompoundStmt();
+        }
+        else if(t == TokenType.IF){
+            return parseSelectionStmt();
+        }
+        else if(t == TokenType.WHILE){
+            return parseIterationStmt();
+        }
+        else if(t == TokenType.RETURN){
+            return parseReturnStmt();
+        }
+        else{
+            //error message
+            return null;
         }
     }
     abstract void print();
