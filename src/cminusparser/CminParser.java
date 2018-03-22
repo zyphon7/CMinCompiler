c451d8aed3a522dac6f7db00ee1d316e6af06d64
Pikapal101 <annie.pikapal@gmail.com>
2018-03-22 19:18:02 -0400

    woot

--- src/cminusparser/CminParser.java
+++ src/cminusparser/CminParser.java
@@ -26,7 +26,7 @@ public class CminParser {
     
     public static void main(String[] args) {
         try{
-            File cminFile = new File("CminPrograms/program5.txt");
+            File cminFile = new File("CminPrograms/program4.txt");
             cminscanner = new CminusScanner(new BufferedReader(new FileReader(cminFile)));
             program = new Program();
             
